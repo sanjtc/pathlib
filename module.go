2931a1b8e7688c0b64a92402e8cc425bf9d9a673
pantskun <wangxin_tongcheng@live.com>
2020-10-21 09:34:46 +0800

    update

--- module.go
+++ module.go
@@ -3,15 +3,13 @@ package pathlib
 import (
 	"os"
 	"strings"
-
-	"github.com/pantskun/pathlib"
 )
 
 // GetModulePath
 // get current module path.
 func GetModulePath(moduleName string) string {
 	fp, _ := os.Getwd()
-	fp = pathlib.ConvertBackslashToSlash(fp)
+	fp = ConvertBackslashToSlash(fp)
 	fp = strings.SplitAfter(fp, moduleName)[0]
 
 	return fp
